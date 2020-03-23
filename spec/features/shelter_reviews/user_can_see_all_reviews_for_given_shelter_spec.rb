ad123f079fb9f31f5b04cd434950fb8df7c16e26
Tyler Porter <tylerpporter@gmail.com>
2020-03-23 15:57:50 -0600

    Create test for reviews to be seen on shelter show page

---
+++ spec/features/shelter_reviews/user_can_see_all_reviews_for_given_shelter_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "when I visit /shelters/:id"
+    it "I see a list of reviews for that shelter" do
+      shelter_1 = Shelter.create(name: "Pallet Town Shelter",
+                          address: "Route 1",
+                          city:  "Pallet Town",
+                          state: "Kanto",
+                          zip: "80807")
+      review1 = ShelterReview.create(title: "This place is great!",
+                              rating: 5,
+                              content: "This shelter treats all of its Pokemon (and customers!) with immense care!",
+                              picture: "https://img.pokemondb.net/artwork/large/caterpie.jpg" )
+      review2 = ShelterReview.create(title: "This place is pretty good!",
+                              rating: 4,
+                              content: "This shelter treats all of its Pokemon pretty good!")
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(shelter_1.reviews).to have_content(review1)
+      expect(shelter_1.reviews).to have_content(review2)
+      expect(page).to have_content(shelter_1.reviews)
+  end
+end
