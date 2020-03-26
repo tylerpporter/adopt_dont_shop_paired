c9c971ffe70d75940c8e7493feadccbae5588b71
Tyler Porter <tylerpporter@gmail.com>
2020-03-25 21:42:04 -0600

    Write test for favorites navbar link

--- spec/features/favorites/user_can_see_all_favorited_pets_spec.rb
+++ spec/features/favorites/user_can_see_all_favorited_pets_spec.rb
@@ -34,4 +34,35 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_content(pet_2.name)
       expect(page).to have_css("img[src*=weedle]")
     end
+    describe "When I click on the favorite indicator in the nav bar"
+      it "I am taken to the favorites index page" do
+        shelter_1 = Shelter.create!(name: "Pallet Town Shelter",
+                            address: "Route 1",
+                            city:  "Pallet Town",
+                            state: "Kanto",
+                            zip: "80807")
+        pet_1 = Pet.create(image: "https://img.pokemondb.net/artwork/large/pidgey.jpg",
+                          name: "Pidgey",
+                          description: "Very gentle and loving",
+                          approx_age:  4,
+                          sex: "Male",
+                          status: "Adoptable",
+                          shelter_id: shelter_1.id)
+        pet_2 = Pet.create(image: "https://img.pokemondb.net/artwork/large/weedle.jpg",
+                          name: "Weedle",
+                          description: "Weed is a loyal and affectionate friend.",
+                          approx_age:  2,
+                          sex: "Male",
+                          status: "Pending",
+                          shelter_id: shelter_1.id)
+
+        visit "/pets/#{pet_1.id}"
+        find("#favorite-#{pet_1.id}").click
+
+        within "nav" do
+          click_link "Favorites"
+        end
+
+        expect(current_path).to eq("/favorites")
+      end 
   end
