843dcb10c2c54ebacc73c138be9bf2bc5ac05f8a
Tyler Porter <tylerpporter@gmail.com>
2020-03-31 19:16:28 -0600

    Create model level test for remove_pet method in Favorite

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Favorite do
   subject { Favorite.new(["12", "25"]) }
 
-
   describe "#total_count" do
     it "calculates the total number of favorited pets" do
       expect(subject.total_count).to eq(2)
@@ -16,5 +15,11 @@ RSpec.describe Favorite do
 
       expect(subject.contents).to eq(["12", "25", "79"])
     end
+  end
+  describe 'instance methods'
+  it 'remove_pet' do
+    subject.remove_pet("25")
+
+    expect(subject.contents).to eq(["12"])
   end
 end
